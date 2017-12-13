20617e955ca05b01f6f16607c880acd19d2db74d
zhh877644371 <877644371@qq.com>
2017-12-13 21:50:30 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { Provider } from 'react-redux';
+import {AppContainer} from 'react-hot-loader';
+import {Provider} from 'react-redux';
 import store from './redux/store';
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from 'components/App/App';
 
-import getRouter from 'router/router';
+renderWithHotReload(App);
 
-/*初始化*/
-renderWithHotReload(getRouter());
-
-/*热更新*/
 if (module.hot) {
-    module.hot.accept('./router/router', () => {
-        const getRouter = require('./router/router').default;
-        renderWithHotReload(getRouter());
+    module.hot.accept('components/App/App', () => {
+        const NextApp = require('components/App/App').default;
+        renderWithHotReload(NextApp);
     });
 }
 
@@ -21,7 +19,9 @@ function renderWithHotReload(RootElement) {
     ReactDom.render(
         <AppContainer>
             <Provider store={store}>
-                {RootElement}
+                <Router>
+                    <RootElement/>
+                </Router>
             </Provider>
         </AppContainer>,
         document.getElementById('app')
